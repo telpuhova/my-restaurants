dd41e34cac46daf0c466fc0d56d86827c681e68c
telpuhova <telpuhova@gmail.com>
2018-03-21 16:00:43 -0700

    add recycler view; add images with picasso

--- app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantsActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantsActivity.java
@@ -1,48 +1,55 @@
 package com.epicodus.myrestaurants.ui;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.epicodus.myrestaurants.R;
-import com.epicodus.myrestaurants.services.YelpService;
+import com.epicodus.myrestaurants.adapters.RestaurantListAdapter;
 import com.epicodus.myrestaurants.models.Restaurant;
+import com.epicodus.myrestaurants.services.YelpService;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import okhttp3.Call;
 import okhttp3.Callback;
+
+import butterknife.ButterKnife;
+import butterknife.BindView;
 import okhttp3.Response;
 
 public class RestaurantsActivity extends AppCompatActivity {
     public static final String TAG = RestaurantsActivity.class.getSimpleName();
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+    private RestaurantListAdapter mAdapter;
 
-    @BindView(R.id.locationTextView) TextView mLocationTextView;
-    @BindView(R.id.listView) ListView mListView;
+
 
     public ArrayList<Restaurant> restaurants = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+
         ButterKnife.bind(this);
+
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
 
-        mLocationTextView.setText("Here are all the restaurants near: " + location);
         getRestaurants(location);
     }
 
-    private void getRestaurants(String location){
+    private void getRestaurants(String location) {
         final YelpService yelpService = new YelpService();
         yelpService.findRestaurants(location, new Callback() {
 
@@ -54,18 +61,20 @@ public class RestaurantsActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) {
                 restaurants = yelpService.processResults(response);
+
                 RestaurantsActivity.this.runOnUiThread(new Runnable() {
+
                     @Override
                     public void run() {
-                        String[] restaurantNames = new String[restaurants.size()];
-                        for (int i = 0; i < restaurantNames.length; i++){
-                            restaurantNames[i] = restaurants.get(i).getName();
-                        }
+                        mAdapter = new RestaurantListAdapter(getApplicationContext(), restaurants);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager =
+                                new LinearLayoutManager(RestaurantsActivity.this);
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
 
-                        ArrayAdapter adapter = new ArrayAdapter(RestaurantsActivity.this, android.R.layout.simple_list_item_1, restaurantNames);
-                        mListView.setAdapter(adapter);
 
-                        for (Restaurant restaurant : restaurants){
+                        for (Restaurant restaurant : restaurants) { //testing purposes only
                             Log.d(TAG, "Name: " + restaurant.getName());
                             Log.d(TAG, "Phone: " + restaurant.getPhone());
                             Log.d(TAG, "Website: " + restaurant.getWebsite());
@@ -75,8 +84,12 @@ public class RestaurantsActivity extends AppCompatActivity {
                             Log.d(TAG, "Categories: " + restaurant.getCategories().toString());
                         }
                     }
+
+
                 });
             }
         });
+
     }
+
 }
